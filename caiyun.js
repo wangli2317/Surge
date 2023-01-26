ce57eaeea3128b7b549ae28b59f448d7ab30016f
wangli2317 <wangli2317@users.noreply.github.com>
2023-01-26 22:52:00 +0800

    Delete caiyun.js

--- caiyun.js
+++
@@ -1,5 +0,0 @@
-var str = JSON.stringify($response.body);
-var body = str.replace(/\"svip_expired_at\":.*?,\"/g, "\"svip_expired_at\":4102415999.5412800312,\"")
-    .replace(/\"vip_type\":\".*?\"/, "\"vip_type\":\"s\"")
-    .replace(/\"is_vip\":.*?,/, "\"is_vip\":true,");
-$done(JSON.parse(body));
